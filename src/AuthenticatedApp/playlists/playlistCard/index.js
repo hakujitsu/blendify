ac6fa04fc96046ee23af53b04ea8c235df76e4bf
Kwek Min Yih <minyihkwek@gmail.com>
2022-11-16 18:57:46 +0800

    Remove ripple effect

--- src/AuthenticatedApp/playlists/playlistCard/index.js
+++ src/AuthenticatedApp/playlists/playlistCard/index.js
@@ -23,9 +23,11 @@ const PlaylistCard = (props) => {
   const [showPlayButton, setShowPlayButton] = useState(false)
 
   return (
-    <Card sx={sx.card} onMouseEnter={() => setShowPlayButton(true)}
-      onMouseLeave={() => setShowPlayButton(false)}>
-      <CardActionArea sx={sx.cardActionArea}>
+    <Card sx={sx.card}
+      onMouseEnter={() => setShowPlayButton(true)}
+      onMouseLeave={() => setShowPlayButton(false)}
+    >
+      <CardActionArea disableRipple sx={sx.cardActionArea}>
         <CardContent sx={sx.cardActionArea}>
           <PlaylistImage img={playlist.images.length > 0 ? playlist.images[0].url : ""} showPlayButton={showPlayButton} />
           <PlaylistDesc title={playlist.title} desc={playlist.description} owner={playlist.owner.display_name} />
--- src/AuthenticatedApp/playlists/playlistCard/playlistImage.js
+++ src/AuthenticatedApp/playlists/playlistCard/playlistImage.js
@@ -1,10 +1,17 @@
-import { Card, CardMedia, Fab, Fade, Paper } from "@mui/material"
+import { Box, Card, CardMedia, Paper } from "@mui/material"
 import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
 import PlayButton from "./playButton";
 
 const sx = {
+  box: {
+    aspectRatio: "1",
+    width: "100%",
+  },
   cardContainer: {
-    position: 'relative'
+    position: "absolute",
+    top: "16px",
+    left: "16px",
+    width: "calc(100% - 32px)",
   },
   cardImage: {
     aspectRatio: "1",
@@ -39,20 +46,22 @@ const PlaylistImage = (props) => {
   const { title, img, showPlayButton } = props
 
   return (
-    <Card elevation={3} sx={sx.cardContainer}>
-      {img ?
-        <CardMedia
-          component="img"
-          image={img}
-          alt={title + " image"}
-          sx={sx.cardImage}
-        /> :
-        <Paper sx={[sx.cardImage, sx.placeholder]}>
-          <LibraryMusicIcon sx={sx.placeholderIcon} />
-        </Paper>
-      }
-      <PlayButton showPlayButton={showPlayButton} title={title} />
-    </Card>
+    <Box sx={sx.box}>
+      <Card elevation={3} sx={sx.cardContainer}>
+        {img ?
+          <CardMedia
+            component="img"
+            image={img}
+            alt={title + " image"}
+            sx={sx.cardImage}
+          /> :
+          <Paper sx={[sx.cardImage, sx.placeholder]}>
+            <LibraryMusicIcon sx={sx.placeholderIcon} />
+          </Paper>
+        }
+        <PlayButton showPlayButton={showPlayButton} title={title} />
+      </Card>
+    </Box>
   )
 }
 
