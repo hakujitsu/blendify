0739c1c86e65d8b4534587e8ad2890d66420cca5
Kwek Min Yih <minyihkwek@gmail.com>
2022-11-17 15:28:24 +0800

    Add play button

--- src/components/playlistSongTable/songRow.js
+++ src/components/playlistSongTable/songRow.js
@@ -1,45 +1,62 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import { convertMsToSongDuration, getDateAddedString } from "../../util/datetime";
 import RowLayout from "./rowLayout";
 import SongTitleBox from "./songTitleCell";
-import React from "react";
+import React, { useState } from "react";
 
 const sx = {
   durationText: {
     pr: 2
+  },
+  playButton: {
+    height: "100%",
+    display: "flex",
+    alignItems: "flex-end",
+    justifyContent: "center",
+    ml: 2,
   }
 }
 
 const SongRow = React.forwardRef((props, ref) => {
   const { song, index, showAlbum, showDate } = props;
+  const [showPlayButton, setShowPlayButton] = useState(false)
 
   return (
-    <div ref={ref}>
+    <div
+      onMouseEnter={() => setShowPlayButton(true)}
+      onMouseLeave={() => setShowPlayButton(false)}
+      ref={ref}
+    >
       <RowLayout
         indexContent={
-          <Typography noWrap align="right">
-            {index + 1}
-          </Typography>
+          showPlayButton ?
+            <Box sx={sx.playButton}>
+              <PlayArrowIcon />
+            </Box> :
+            <Typography noWrap align="right">
+              {index + 1}
+            </Typography>
         }
         titleContent={
-          <SongTitleBox image={song.track.album.images.slice(-1)[0].url} title={song.track.name} artists={song.track.artists} />
+          < SongTitleBox image={song.track.album.images.slice(-1)[0].url} title={song.track.name} artists={song.track.artists} />
         }
-        albumContent={<Typography variant="body2" noWrap>{song?.track?.album.name}</Typography>}
-        dateContent={<Typography variant="body2" noWrap>{getDateAddedString(song?.added_at)}</Typography>}
+        albumContent={< Typography variant="body2" noWrap > {song?.track?.album.name}</Typography >}
+        dateContent={< Typography variant="body2" noWrap > {getDateAddedString(song?.added_at)}</Typography >}
         likedContent={
           song?.track.liked ? <FavoriteIcon fontSize="small" /> : <FavoriteBorderIcon fontSize="small" />
         }
         durationContent={
-          <Typography variant="body2" noWrap align="right" sx={sx.durationText}>
+          < Typography variant="body2" noWrap align="right" sx={sx.durationText} >
             {convertMsToSongDuration(song?.track.duration_ms)}
-          </Typography>
+          </Typography >
         }
         showAlbum={showAlbum}
         showDate={showDate}
       />
-    </div>
+    </div >
   )
 })
 
